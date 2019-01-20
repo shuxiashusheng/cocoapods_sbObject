1f3624784e8f09f2b2c8edae1894ee88f8ae8c41
shaobo10 <shaobo10@staff.sina.com.cn>
2019-01-21 07:55:40 +0800

    ew

---
+++ cocoapods_sbObject/NSObject+sbObject.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+sbObject.h
+//  
+//
+//  Created by shaobo10 on 2019/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (sbObject)
+- (id)sbLog;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cocoapods_sbObject/NSObject+sbObject.m
@@ -0,0 +1,14 @@
+//
+//  NSObject+sbObject.m
+//  
+//
+//  Created by shaobo10 on 2019/1/21.
+//
+
+#import "NSObject+sbObject.h"
+
+@implementation NSObject (sbObject)
+- (id)sbLog {
+    return @"sblog";
+}
+@end
